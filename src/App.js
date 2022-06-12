aeefe7f7f6d9b88e28de70ec7afdff4d8317b00e
tuhuynh <tuhuynh1902@gmail.com>
2022-06-13 00:00:10 +0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { increaseCounter, decreaseCounter } from './action/actions';
+import { useEffect } from 'react';
 
 function App(props) {
+  const dispatch = useDispatch();
+  const newCount = useSelector((state) => state.counter.count);
+
+  const handleIncrease = () => {
+    dispatch(increaseCounter());
+  };
+
+  const fetchAllUsers = async () => {
+    const res = await axios.get('http://localhost:8080/users/all');
+    const data = res && res.data ? res.data : [];
+    console.log('check data>>>>', data);
+  };
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and Hoi Dan IT!</h1>
-        <div>Count: {props.count}</div>
+        <div>Count: {newCount}</div>
 
-        <button onClick={() => props.increaseCounter()}>Increase Count</button>
+        <button onClick={() => handleIncrease()}>Increase Count</button>
 
-        <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
       </header>
     </div>
   );
